4dc47e6c90d0254951cbfbe2bc043749d5a26a92
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-03-16 08:38:19 +1100

    Implement initial fizzbuzz failing test Update tsconfig so that it understands Jest

--- 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/1 - Red-Green-Refactor Practice/1.1 - Fizzbuzz/src/index.spec.ts
@@ -1,2 +1,7 @@
+import { fizzbuzz } from "./fizzbuzz"
 
-describe("fizzbuzz", () => { });
+describe("fizzbuzz", () => {
+  it("Should return a string", () => {
+    expect(typeof fizzbuzz()).toEqual("string")
+  })
+});
