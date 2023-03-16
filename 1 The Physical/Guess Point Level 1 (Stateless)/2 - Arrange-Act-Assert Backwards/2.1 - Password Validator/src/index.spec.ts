69a334009cc9fbfcac8e3a51623c950249909de9
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-03-17 09:33:56 +1100

    Password validator: Move TextValidation and update test cases Move TextValidation into its own file and implement specific tests Update test cases for password validation

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,22 +1,17 @@
 import { Password } from "./index"
+
+const invalidLengthError = { type: "INVALID_LENGTH", message: "Password must be between 1 and 15 characters" };
+const invalidFormatNoDigit = { type: "INVALID_FORMAT", message: "Password contain at least 1 digit" };
+const invalidFormateNoUpperCase = { type: "INVALID_FORMAT", message: "Password contain at least 1 uppercase character" };
+
 describe('password validator', () => {
   describe("Given the password validator is provided a password", () => {
-    const lengthTestCases: [string, boolean][] = [
-      ["s", false],
-      ["passwordistoomanycharacterssorry", false],
-      ["Password1", true]
+    const lengthTestCases: [string, { valid: boolean, errors: { type: string, message: string }[] }][] = [
+      ["s", { valid: false, errors: [invalidLengthError, invalidFormatNoDigit, invalidFormateNoUpperCase] }],
+      ["passwordistoomanycharacterssorry", { valid: false, errors: [invalidLengthError, invalidFormatNoDigit, invalidFormateNoUpperCase] }],
+      ["Password1", { valid: true, errors: [] }]
     ]
     it.each(lengthTestCases)("Then it should be between 5 and 15 characters %s", (password, expected) => {
-      expect(Password.validate(password)).toEqual(expected)
-    })
-  })
-  describe("Given the password validator is provided a password", () => {
-    const formatTestCases: [string, boolean][] = [
-      ["s", false],
-      ["1", false],
-      ["Password1", true]
-    ]
-    it.each(formatTestCases)("Then it should contain at least 1 digit and 1 uppercase character %s", (password, expected) => {
       expect(Password.validate(password)).toEqual(expected)
     })
   })
--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.ts
@@ -1,21 +1,10 @@
-class TextValidation {
-  static hasUpperCase(password: string) {
-    return /[A-Z]/.test(password)
-  }
-  static hasDigit(password: string) {
-    return /d/.test(password)
-  }
-  static hasLength(password: string, min: number, max: number) {
-    return password.length >= min && password.length <= max
-  }
-}
-
+import { TextValidation } from "./TextValidation"
 export class Password {
   static validate(password: string) {
     return (
       TextValidation.hasDigit(password) &&
       TextValidation.hasUpperCase(password) &&
-      TextValidation.hasLength(password, 5, 15)
+      TextValidation.hasLength(password)
     );
   }
 }
