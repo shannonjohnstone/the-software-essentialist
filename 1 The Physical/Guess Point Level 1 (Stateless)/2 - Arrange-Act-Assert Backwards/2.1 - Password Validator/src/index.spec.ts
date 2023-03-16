304c6f46ad79ff8aabc3f9aca70e88098ef412b6
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-03-17 07:13:01 +1100

    Password Validator: Implement basic test cases and basic empty function

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.1 - Password Validator/src/index.spec.ts
@@ -1,5 +1,17 @@
-
+import { passwordValidator } from "./index"
 describe('password validator', () => {
+  describe("Given the password validator is provided a password", () => {
+    it("Then it should be between 5 and 15 characters", () => {
+      expect(passwordValidator("password")).toEqual(true)
+    })
 
+    const testCases: [string, boolean][] = [
+      ["s", false],
+      ["1", false],
+      ["1S", true]
+    ]
+    it.each(testCases)("Then it should contain at least 1 digit and 1 uppercase character", (password, expected) => {
+      expect(passwordValidator(password)).toEqual(expected)
+    })
+  })
 })
-
