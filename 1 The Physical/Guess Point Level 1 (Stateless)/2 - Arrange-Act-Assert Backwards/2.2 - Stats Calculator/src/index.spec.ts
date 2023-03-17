87dc7d3fe0756fe6c7ebf33076b3641d640430b4
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-03-17 15:56:09 +1100

    Stats Calculator: Implement basic stats calculator and test use cases

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -1,5 +1,15 @@
-
+import exp from "constants"
+import { StatsCalculator } from "./index"
 describe('stats calculator', () => {
-
-
+  const testCases: [string, number[], Function, number][] = [
+    ["minimum", [2, 4, 21, -8, 53, 40], StatsCalculator.minimum, - 8],
+    ["maximum", [2, 4, 21, -8, 53, 40], StatsCalculator.maximum, 40],
+    ["elementsCount", [2, 4, 21, -8, 53, 40], StatsCalculator.elementsCount, 6],
+    ["average", [2, 4, 21, -8, 53, 40], StatsCalculator.average, 18.666666666667],
+  ]
+  describe.each(testCases)("Given the %s method on the StatsCalculator is invoked with the values of %6", (_, numbers, statsMethod, expected) => {
+    it("Then should return a response of %s", () => {
+      expect(statsMethod(numbers)).toEqual(expected)
+    })
+  })
 })
---
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -0,0 +1,17 @@
+export class StatsCalculator {
+  static minimum(stats: number[]): number {
+    return 0
+  }
+
+  static maximum(stats: number[]): number {
+    return 0
+  }
+
+  static elementsCount(stats: number[]): number {
+    return 0
+  }
+
+  static average(stats: number[]): number {
+    return 0
+  }
+}
