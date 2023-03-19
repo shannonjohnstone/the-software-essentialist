67df13eddaebe193678ba3708afe136be163ab27
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-03-20 05:54:54 +1100

    Stats calculator: Implement Stats methods and test cases

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.ts
@@ -1,17 +1,17 @@
 export class StatsCalculator {
   static minimum(stats: number[]): number {
-    return 0
+    return stats.reduce((min, num) => num < min ? num : min, stats[0])
   }
 
   static maximum(stats: number[]): number {
-    return 0
+    return stats.reduce((max, num) => num > max ? num : max, stats[0])
   }
 
   static elementsCount(stats: number[]): number {
-    return 0
+    return stats.length
   }
 
   static average(stats: number[]): number {
-    return 0
+    return stats.reduce((acc, num) => acc + num, 0) / stats.length
   }
 }
