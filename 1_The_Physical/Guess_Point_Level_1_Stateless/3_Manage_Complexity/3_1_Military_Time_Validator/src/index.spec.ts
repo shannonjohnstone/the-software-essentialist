b5338eb0c19f9138cee4ac169425a0d99c36e286
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-03-20 16:18:50 +1100

    Time Validator [Improvement]: update  TimeValidator class with working regex and string normalisation and passing use case

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,6 +2,6 @@ import { TimeValidator } from "./index"
 
 describe('TimeValidator', () => {
   it("Should be truthy", () => {
-    expect(TimeValidator.is24HourTime()).toEqual(true)
+    expect(TimeValidator.isValid24HourTimeRange("01:12 - 14:32")).toEqual(true)
   })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,5 +1,15 @@
 export class TimeValidator {
-  static is24HourTime() {
-    return true;
+  private static normalizeTimeRange(range: string, delimiter: string): [string, string] {
+    const [start = "", end = ""] = range.split(delimiter)
+    return [start.trim(), end.trim()]
+  }
+  static isValid24HourTimeRange(range: string): boolean {
+    const [start, end] = TimeValidator.normalizeTimeRange(range, "-")
+
+    if (!start && !end) return false;
+
+    const is24HourTimeFormatRegex = /^(?:[01][0-9]|2[0-3]):[0-5][0-9](?::[0-5][0-9])?$/;
+
+    return is24HourTimeFormatRegex.test(start) && is24HourTimeFormatRegex.test(end);
   }
 }
