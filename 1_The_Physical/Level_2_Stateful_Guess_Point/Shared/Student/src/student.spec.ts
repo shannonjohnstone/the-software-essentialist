79f7b79e994cf43ec76fdaaac8a701a8be1351e6
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-05-07 11:09:53 +1000

    Implement basic Student creation for Error type

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/student.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/student.spec.ts
@@ -1,14 +1,26 @@
 import { Student } from "./student";
 
 describe("Student", () => {
-  describe("Given a student is created", () => {
-    it("Then a studen object is created", () => {
+  describe("Given a valid student is created", () => {
+    it("Then a student object is created", () => {
       const studentObject = Student.create({
         firstName: "John",
         lastName: "Smith",
       });
 
       expect(studentObject).toBeInstanceOf(Student);
+    });
+  });
+
+  describe("Given a invalid student is created", () => {
+    it("Then a error object is created", () => {
+      const studentObject = Student.create({ firstName: "", lastName: "" });
+
+      expect(studentObject).toBeInstanceOf(Array);
+      expect(studentObject).toEqual([
+        { message: "Invalid firstName value", type: "INVALID_FIRSTNAME" },
+        { message: "Invalid lastName value", type: "INVALID_LASTNAME" },
+      ]);
     });
   });
 });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/student.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/student.ts
@@ -3,10 +3,44 @@ interface StudentProps {
   lastName: string;
 }
 
+enum ErrorTypeEnum {
+  firstName = "INVALID_FIRSTNAME",
+  lastName = "INVALID_LASTNAME",
+}
+
+interface Error {
+  type: ErrorTypeEnum.firstName | ErrorTypeEnum.lastName;
+  message: string;
+}
+
 export class Student {
   constructor(private studentProps: StudentProps) { }
 
-  static create(studentProps: StudentProps) {
+  private static validateNameProps(studentProps: StudentProps): Error[] {
+    const errors: Error[] = [];
+
+    type Keys = Extract<keyof StudentProps, string>;
+
+    Object.keys(studentProps).forEach((key) => {
+      const value = studentProps[key as Keys];
+
+      if (!value.length && value.length <= 10) {
+        const type = ErrorTypeEnum[key as Keys] || `INVALID_VALUE`;
+
+        errors.push({
+          type,
+          message: `Invalid ${key} value`,
+        });
+      }
+    });
+
+    return errors;
+  }
+
+  static create(studentProps: StudentProps): Student | Error[] {
+    const errors = Student.validateNameProps(studentProps);
+    if (errors.length) return errors;
+
     return new Student(studentProps);
   }
 }
