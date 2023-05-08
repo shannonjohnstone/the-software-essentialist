057c485056cc0323df86b4325616413417084ceb
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-05-09 09:19:49 +1000

    First name value object Update test cases Implement typed error implementation Implement use of validator

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/first-name.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/first-name.spec.ts
@@ -1,17 +1,29 @@
 import { FirstName } from "./first-name";
+import { Validator } from "./validator";
 
 describe("First name", () => {
   describe("Given a first name is created", () => {
     it("Then there should be a valid first name instance", () => {
-      const firstName = FirstName.create("John");
+      const firstName = FirstName.create("John", Validator.validator);
       expect(firstName).toBeInstanceOf(FirstName);
     });
   });
 
-  describe("Given a first name is created", () => {
-    it("Then get name value", () => {
-      const firstName = FirstName.create("John");
+  describe("Given a valid first name of 'John' is created", () => {
+    it("Then a value of 'John' is retruned", () => {
+      const firstName = FirstName.create("John", Validator.validator);
       expect(firstName.value).toEqual("John");
+    });
+  });
+
+  describe("Given a invalid first name of '' is used", () => {
+    it("Then return error", () => {
+      const { error } = FirstName.create("", Validator.validator);
+
+      expect(error).toEqual({
+        message: 'Invalid first name value of ""',
+        type: "INVALID_FIRSTNAME",
+      });
     });
   });
 });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/first-name.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/first-name.ts
@@ -1,12 +1,43 @@
-type Name = string;
-export class FirstName {
-  constructor(private name: Name) { }
+import { ValidationError } from "./validator";
 
-  static create(name: Name): FirstName {
-    return new FirstName(name);
+type Name = string;
+
+interface Entity<Props> {
+  value: Props;
+  error?: ValidationError | void;
+}
+
+type Validator = ({
+  value,
+  pattern,
+}: {
+  value: string;
+  pattern: RegExp;
+}) => boolean;
+
+export class FirstName implements Entity<Name> {
+  private pattern = /^[a-z]{1,10}$/i;
+
+  constructor(private name: Name, private validator: Validator) { }
+
+  static create(name: Name, validator: Validator): FirstName {
+    return new FirstName(name, validator);
   }
 
-  get value(): Name {
+  get error() {
+    const { validator, name, pattern } = this;
+
+    const isInvalid = !validator({ value: name, pattern });
+
+    if (isInvalid) {
+      return {
+        type: "INVALID_FIRSTNAME",
+        message: `Invalid first name value of "${name}"`,
+      };
+    }
+  }
+
+  get value() {
     return this.name;
   }
 }
