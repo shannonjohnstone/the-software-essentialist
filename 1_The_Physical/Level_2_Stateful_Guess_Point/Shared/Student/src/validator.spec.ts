11cf28de22633d4c167fe7c81462c32674248222
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-05-09 09:13:34 +1000

    Validator updates Update test cases Implement initial validator metthod to validate single values

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/validator.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/validator.spec.ts
@@ -1,10 +1,48 @@
 import { Validator } from "./validator";
 
 describe("Validator", () => {
-  describe("Given a validator is created", () => {
-    it("Then returns a validator instance", () => {
-      const validator = Validator.create();
-      expect(validator).toBeInstanceOf(Validator);
+  describe("Given the validate is ran with invalidate values", () => {
+    it("Then return validation errors", () => {
+      class ValidatorMock1 {
+        get error() {
+          return {
+            type: "INVALID_MOCK_ONE",
+            message: "Invalid mock one",
+          };
+        }
+      }
+
+      const mock1 = new ValidatorMock1();
+      const validator = Validator.validate([mock1.error]);
+
+      expect(validator).toEqual([
+        {
+          type: "INVALID_MOCK_ONE",
+          message: "Invalid mock one",
+        },
+      ]);
+    });
+  });
+
+  describe("Given the validator is ran with '' and '/^[a-z]{1,10}$/i", () => {
+    it("Then a false result", () => {
+      const validator = Validator.validator({
+        value: "",
+        pattern: /^[a-z]{1,10}$/i,
+      });
+
+      expect(validator).toEqual(false);
+    });
+  });
+
+  describe("Given the validator is ran with 'John' and '/^[a-z]{1,10}$/i", () => {
+    it("Then a true result", () => {
+      const validator = Validator.validator({
+        value: "John",
+        pattern: /^[a-z]{1,10}$/i,
+      });
+
+      expect(validator).toEqual(true);
     });
   });
 });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/validator.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/validator.ts
@@ -1,5 +1,22 @@
+export interface ValidationError {
+  type: string;
+  message: string;
+}
+
+export interface ValidatorProps {
+  value: string;
+  pattern: RegExp;
+}
+
+type ValidatorItem = ValidationError;
+
 export class Validator {
-  static create(): Validator {
-    return new Validator();
+  static validate(items: ValidatorItem[]): ValidationError[] {
+    return items.map((validator) => validator).filter(Boolean);
+  }
+
+  static validator({ value, pattern }: ValidatorProps) {
+    const regex = new RegExp(pattern);
+    return !!value || regex.test(value);
   }
 }
