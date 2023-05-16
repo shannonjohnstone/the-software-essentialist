35c2abc8bb73b4024c1640902a664256ec5794b5
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-05-17 09:17:19 +1000

    Implement makeMove Implement makeMove method Update types for board type Implement getPlayerAtPosition method Implement test cases

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.spec.ts
@@ -23,3 +23,13 @@ describe("Given a game is started", () => {
     expect(game.getCurrentPlayer).toEqual("X");
   });
 });
+
+describe("Given a game is started", () => {
+  it("Then player X makes the first move in position 0", () => {
+    const game = TicTacToe.start();
+
+    game.makeMove(0);
+
+    expect(game.getPlayerAtPostion(0)).toEqual("X");
+  });
+});
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.ts
@@ -1,12 +1,12 @@
 enum Players {
-  X = "X",
-  O = "O",
+  "X" = "X",
+  "O" = "O",
 }
 
 type Player = `${Players}`;
 
 export class TicTacToe {
-  readonly board: unknown[];
+  public board: (Player | null)[];
   readonly currentPlayer: Player = Players.X;
 
   constructor() {
@@ -17,11 +17,23 @@ export class TicTacToe {
     return new TicTacToe();
   }
 
+  makeMove(position: number) {
+    const player = this.getCurrentPlayer;
+
+    if (!this.board[position]) {
+      this.board[position] = player;
+    }
+  }
+
   get getAmountOfBoardSquares() {
     return this.board.length;
   }
 
   get getCurrentPlayer() {
     return this.currentPlayer;
+  }
+
+  getPlayerAtPostion(position: number) {
+    return this.board[position];
   }
 }
