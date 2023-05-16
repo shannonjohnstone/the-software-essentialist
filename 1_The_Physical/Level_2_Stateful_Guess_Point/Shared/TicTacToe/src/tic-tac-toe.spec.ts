bbb7a1331d7e09006f58bc1c5f6025fc921fdf55
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-05-17 08:54:52 +1000

    Player type and current player Implement basic player type Implement getCurrentPlayer method Implement default current player to be X

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.spec.ts
@@ -1,17 +1,25 @@
-import { TicTacToe } from "./tic-tac-toe"
+import { TicTacToe } from "./tic-tac-toe";
 
-describe('Given a game is started', () => {
+describe("Given a game is started", () => {
   it("Then a game instance should create", () => {
-    const game = TicTacToe.start()
+    const game = TicTacToe.start();
 
-    expect(game).toBeInstanceOf(TicTacToe)
-  })
-})
+    expect(game).toBeInstanceOf(TicTacToe);
+  });
+});
 
-describe('Given a game is started', () => {
+describe("Given a game is started", () => {
   it("Then is 9 spaces on the board", () => {
-    const game = TicTacToe.start()
+    const game = TicTacToe.start();
 
-    expect(game.getAmountOfBoardSquares).toEqual(9)
-  })
-})
+    expect(game.getAmountOfBoardSquares).toEqual(9);
+  });
+});
+
+describe("Given a game is started", () => {
+  it("Then player X is the current player", () => {
+    const game = TicTacToe.start();
+
+    expect(game.getCurrentPlayer).toEqual("X");
+  });
+});
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.ts
@@ -1,16 +1,27 @@
+enum Players {
+  X = "X",
+  O = "O",
+}
+
+type Player = `${Players}`;
 
 export class TicTacToe {
-  readonly board: unknown[]
+  readonly board: unknown[];
+  readonly currentPlayer: Player = Players.X;
 
   constructor() {
-    this.board = Array(9).fill(null)
+    this.board = Array(9).fill(null);
   }
 
   static start() {
-    return new TicTacToe()
+    return new TicTacToe();
   }
 
   get getAmountOfBoardSquares() {
-    return this.board.length
+    return this.board.length;
+  }
+
+  get getCurrentPlayer() {
+    return this.currentPlayer;
   }
 }
