2d3105262b8bb440b5012eb73b815c44ca526000
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2023-05-17 10:56:26 +1000

    Implement check for a winner and game status Implement getStatus to check the current status of the game Implement getWinner to check which player is the winner

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.spec.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import { TicTacToe } from "./tic-tac-toe";
 
 describe("Given a game is started", () => {
@@ -41,5 +42,50 @@ describe("Given player X makes a vald move", () => {
     game.makeMove(0);
 
     expect(game.getCurrentPlayer).toEqual("O");
+  });
+});
+
+describe("Given player X made winning moves", () => {
+  it.each([
+    { a: [0, 1, 2], b: [3, 4, 6], expected: { isOver: true, player: "X" } },
+    { a: [3, 4, 5], b: [1, 2, 6], expected: { isOver: true, player: "X" } },
+    { a: [6, 7, 8], b: [1, 2, 4], expected: { isOver: true, player: "X" } },
+    { a: [0, 3, 6], b: [1, 2, 5], expected: { isOver: true, player: "X" } },
+    { a: [1, 4, 7], b: [2, 3, 5], expected: { isOver: true, player: "X" } },
+    { a: [2, 5, 8], b: [1, 3, 6], expected: { isOver: true, player: "X" } },
+    { a: [0, 4, 8], b: [1, 2, 4], expected: { isOver: true, player: "X" } },
+    { a: [2, 4, 6], b: [1, 3, 5], expected: { isOver: true, player: "X" } },
+  ])(
+    "Then the game should be in a status of over with a winning player, with the positions of $a and $b have been played",
+    ({ a, b, expected }) => {
+      const game = TicTacToe.start();
+
+      Array(3)
+        .fill(null)
+        .forEach((_, index) => {
+          game.makeMove(a[index]);
+          game.makeMove(b[index]);
+        });
+
+      expect(game.gameStatus).toEqual(expected);
+    }
+  );
+
+  describe("Given all turns have bee had but no player has 3 in a row", () => {
+    it("Then the game is over without a winnder", () => {
+      const game = TicTacToe.start();
+
+      game.makeMove(0);
+      game.makeMove(1);
+      game.makeMove(2);
+      game.makeMove(3);
+      game.makeMove(4);
+      game.makeMove(8);
+      game.makeMove(7);
+      game.makeMove(6);
+      game.makeMove(5);
+
+      expect(game.gameStatus).toEqual({ isOver: true });
+    });
   });
 });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/tic-tac-toe.ts
@@ -8,6 +8,16 @@ type Player = `${Players}`;
 export class TicTacToe {
   private board: (Player | null)[];
   private currentPlayer: Player = Players.X;
+  private winningPositions: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   constructor() {
     this.board = Array(9).fill(null);
@@ -26,6 +36,33 @@ export class TicTacToe {
     } else {
       // Come back to handling error
     }
+  }
+
+  get getWinner(): Player | null {
+    for (const [a, b, c] of this.winningPositions) {
+      const firstPosition = this.board[a];
+      if (
+        firstPosition &&
+        this.board[a] === this.board[b] &&
+        this.board[a] === this.board[c]
+      ) {
+        return this.board[a];
+      }
+    }
+
+    return null;
+  }
+
+  get gameStatus(): { isOver: boolean; player?: Player } {
+    const allPositionsTaken = !this.board.includes(null);
+
+    if (this.getWinner) {
+      return { isOver: true, player: this.getWinner };
+    }
+
+    return allPositionsTaken && !this.getWinner
+      ? { isOver: true }
+      : { isOver: false };
   }
 
   get getAmountOfBoardSquares() {
